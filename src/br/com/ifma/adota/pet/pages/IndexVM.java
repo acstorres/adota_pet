2d6ff47b628699c94df9707c44f0b836264c62dd
acstorres <torres.devsi@gmail.com>
2019-08-12 11:02:22 -0300

    terceiro

--- src/br/com/ifma/adota/pet/pages/IndexVM.java
+++ src/br/com/ifma/adota/pet/pages/IndexVM.java
@@ -64,4 +64,36 @@ public class IndexVM {
 		Selectors.wireComponents(view, this, false);
 	}
 
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getConteudo() {
+		return conteudo;
+	}
+
+	public void setConteudo(String conteudo) {
+		this.conteudo = conteudo;
+	}
+
 }
